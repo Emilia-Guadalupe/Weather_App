ffc993e11a7c91bdd709def591324180c64afcd5
Emilia <m.emilia.guadalupe@gmail.com>
2021-03-20 19:20:40 -0300

    Added the final Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Forecast from './Components/Forecast/Forecast';
+import Logo from './Components/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <><Logo /></>
         <h1>Weather App</h1>
       </header>
       <main>
