7b63140156791fc42727e71a7c3bba07432f21d3
Emilia <m.emilia.guadalupe@gmail.com>
2021-03-20 19:21:30 -0300

    Finishing Up!

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -1,24 +1,53 @@
 import React, {useState} from 'react';
 import Conditions from '../Conditions/Conditions';
+import './forecastStyles.css';
 
 function Forecast(){
 
+    const [city, setCity] = useState('');
+    const [unit, setUnit] = useState('imperial');
     const [responseObj, setResponseObj] = useState({});
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    function getForecast(){
+    const API_KEY = process.env.REACT_APP_API_KEY; 
+
+    function getForecast(e){
+        e.preventDefault();
+
+        if(city.length === 0) {
+            return setError(true);
+        }
+
+        //Clear state in preparation for new data
+
+        setError(false);
+        setResponseObj({});
+
+        setLoading(true);
+
+        const uriEncodedCity = encodeURIComponent(city);
+
         //Weather data fetch function will go here
-        fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=seattle", {
+        fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
 	"method": "GET",
 	"headers": {
-	"x-rapidapi-key": "ab1adc361fmsha9a75b5caa148fep15801cjsnd4d0717bf4ca",
+	"x-rapidapi-key": API_KEY,
 	"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
 	}
 })
 .then(response => response.json())
 .then(response => {
+
+        if(response.cod !== 200){
+            throw new Error();
+        }
     setResponseObj(response);
+    setLoading(false);
 })
 .catch(err => {
+    setError(true);
+    setLoading(true);
 	console.error(err);
 });
 
@@ -28,10 +57,27 @@ function Forecast(){
         <>
         <div>
             <h2>Find Current Weather Conditions</h2>
-            <button onClick={getForecast}>Get Forecast</button>
         </div>
         <div>
-            <Conditions responseObj={responseObj} />
+        <form onSubmit={getForecast}>
+                <input  className="textInput" type="text" placeholder="Enter City" maxLength="50" value={city} onChange={(e) => setCity(e.target.value)} />
+                <label className="radio">
+                    <input type="radio" name="units" checked={unit === "imperial"} value="imperial" onChange={(e) => setUnit(e.target.value)} />
+                    Fahrenheit
+                </label>
+                <label className="radio">
+                    <input type="radio" name="units" checked={unit === "metric"} value="metric" onChange={(e) => setUnit(e.target.value)} />
+                    Celcius
+                </label>
+                <button  className="button" type="submit">Get Forecast</button>
+            </form>
+        </div>
+        <div>
+            <Conditions 
+            responseObj={responseObj}
+            error={error}
+            loading={loading}
+            />
         </div>
         </>
     )
---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,18 @@
+// Logo.js
+import React from 'react';
+import classes from "./logoStyles.css";
+
+function Logo(){
+    return(
+        <>
+        <div className="sky">
+        <div className="circle" />
+        <div className="cloud1" />
+        <div className="cloud2" />
+        <div className="cloud3" />
+        </div>
+        </>
+    )
+}
+
+export default Logo;
