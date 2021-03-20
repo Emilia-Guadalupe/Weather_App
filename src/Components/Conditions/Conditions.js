3166746d2bec1b8d723e66e85f34b46b0df565a3
Emilia <m.emilia.guadalupe@gmail.com>
2021-03-20 19:19:56 -0300

    Finished the Component

--- src/Components/Conditions/Conditions.js
+++ src/Components/Conditions/Conditions.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import './conditionsStyles.css';
 
 function Conditions(props){
     return(
-        <div>
-            <div>
+        <>
+            <div className="wrapper">
+                {props.error && <small className="small">Please enter a valid city.</small>}
+                {props.loading && <div className="loader">Loading...</div>}
                 {props.responseObj.cod === 200 ? 
                 <div>
                     <p>{props.responseObj.name}</p>
                     <p>It's currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
                 </div> : null} 
             </div>
-        </div>
+        </>
     )
 }
 
